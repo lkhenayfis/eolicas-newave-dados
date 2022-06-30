4b5b77a3902efdbcf0527a638a693db77159a771
Lucas De Souza Khenayfis <lucask@ons.org.br>
2022-06-30 12:37:25 -0300

    Inclui script para clusterizacao de usinas

---
+++ main/clusteriza_usinas.r
@@ -0,0 +1,67 @@
+library(data.table)
+library(clustcens)
+library(ggplot2)
+
+source("R/utils.r")
+
+# INICIALIZACAO ------------------------------------------------------------------------------------
+
+arq_conf <- commandArgs(trailingOnly = TRUE)[1]
+if(is.na(arq_conf)) arq_conf <- "conf/default/clusteriza_usinas_default.jsonc"
+
+CONF <- jsonlite::read_json(arq_conf, TRUE)
+CONF$Modelos <- lapply(CONF$Modelos, function(l) {
+    l[[1]] <- paste0("clust", l[[1]])
+    l[[1]] <- str2lang(l[[1]])
+    as.call(l)
+})
+
+shape <- readRDS("data/shape.rds")
+
+dat_usinas <- readRDS("data/usinas.rds")
+usi_comhist <- sub(".rds", "", list.files("data/mhg"))
+dat_usinas <- dat_usinas[codigo %in% usi_comhist]
+
+dir.create("out/clusteriza_usinas", recursive = TRUE)
+
+# EXECUCAO PRINCIPAL -------------------------------------------------------------------------------
+
+index_loop <- expand.grid(mod = names(CONF$Modelos), subsist = CONF$Subsistemas,
+    stringsAsFactors = FALSE)
+
+track <- ""
+
+for(i in seq(nrow(index_loop))) {
+
+    subsist <- index_loop$subsist[i]
+    mod     <- index_loop$mod[i]
+
+    if(track != subsist) {
+        rean_mensal <- readRDS(file.path("data", paste0("reanalise_", subsist, ".rds")))
+        rean_mensal[, grupo := subsist]
+        colnames(rean_mensal)[1:3] <- c("indice", "valor", "cenario")
+        rean_mensal <- clustcens:::new_cenarios(rean_mensal)
+
+        compac <- PCAcens(rean_mensal, .8)
+        compac$compact[, valor := scale(valor), by = .(ind)]
+    }
+
+    clust <- CONF$Modelos[[index_loop$mod[i]]]
+    clust$compact <- quote(compac)
+    clust <- eval(clust)
+
+    classe <- getclustclass(clust)
+
+    usiplot <- merge(dat_usinas,
+        data.table(codigo = unique(rean_mensal$cenarios$cenario),
+            cluster = paste0("cluster_", classe = classe)))
+
+    gg <- ggplot() +
+        geom_polygon(data = shape, aes(long, lat, group = group), fill = NA, color = "grey60") +
+        geom_point(data = usiplot, aes(longitude, latitude, color = cluster)) +
+        coord_cartesian(xlim = range(usiplot$longitude), ylim = range(usiplot$latitude)) +
+        labs(title = index_loop$mod[i]) +
+        theme_bw()
+    ggsave(file.path("out/clusteriza_usinas", paste0(index_loop$mod[i], "_", subsist, ".png")), gg,
+        width = 8, height = 8)
+}
