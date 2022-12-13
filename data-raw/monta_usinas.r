6a92d44077d3299900bf9159eb4092285c44af9f
Lucas de Souza Khenayfis <lucask@ons.org.br>
2022-12-13 16:10:23 -0300

    Tira trecho comentado

--- data-raw/monta_usinas.r
+++ data-raw/monta_usinas.r
@@ -1,27 +1,5 @@
 library(data.table)
 
-#usinas <- fread("data-raw/Dados das Usinas.txt")
-#usinas[] <- lapply(usinas, trimws)
-#usinas[, codigo := sub("_.*", "", V1)]
-#usinas[, nome := sub(".{6}_", "", V1)]
-#usinas[, c(2:6, 8, 10)] <- lapply(usinas[, c(2:6, 8, 10)], as.numeric)
-#usinas[, V1 := NULL]
-#names(usinas) <- c("capacidade_instalada", "latitude", "longitude", "data_inicio_operacao",
-#    "simulada", "subsistema_eletrico", "data_inicio_simulacao", "subsistema_geografico", "B",
-#    "codigo", "nome")
-#usinas[, id := seq(.N)]
-#setcolorder(usinas,
-#    c("id", "codigo", "nome", "capacidade_instalada", "latitude", "longitude", "data_inicio_operacao",
-#    "data_inicio_simulacao", "simulada", "subsistema_geografico", "subsistema_eletrico", "B")
-#)
-#
-#usinas[, data_inicio_operacao := as.Date(as.character(data_inicio_operacao), format = "%Y%m%d")]
-#usinas[, data_inicio_simulacao := as.Date(as.character(data_inicio_simulacao), format = "%Y%m%d")]
-#usinas[, B := NULL]
-#usinas[, nome := gsub("_", " ", nome)]
-
-# --------------------------------------------------------------------------------------------------
-
 shape <- readRDS("data/shape.rds")
 setDT(shape)
 shape[, group := as.numeric(as.character(group))]
